e15bd41298c0a6849b247c0fe448ab8d8b65e8c6
hishamrehab <hishamrehab30@gmail.com>
2025-10-27 14:18:54 +0300

    Search Bar

---
+++ 1-react-hooks-basic-projects/react-demos/6-SearchBar/components/HiddenSearchBar.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import {FaSearch} from 'react-icons/fa'
+
+const HiddenSearchBar = () => {
+    const [showInput , setShowInput] = useState(false);
+    const [bgColor , setBgColor] = useState("white");
+
+    const handleClick = (e) => {
+        setBgColor("#1a1a1a");
+
+
+        if(e.target.className === "container"){
+            setShowInput(false);
+         setBgColor("white");
+        }
+    }
+
+  return (
+    <section className='container' style={{
+       backgroundColor : bgColor  
+    }}
+     onClick={handleClick}
+    >
+ {showInput ? (
+    <input type='text' placeholder='Search...' />
+ ) : (
+    <FaSearch onClick={setShowInput(true)} />
+ )}
+    </section>
+  )
+}
+
+export default HiddenSearchBar
