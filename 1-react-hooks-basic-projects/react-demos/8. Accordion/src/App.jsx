5509720b6feb9ab809f010e795fce37af83af6cd
hishamrehab <hishamrehab30@gmail.com>
2025-10-27 21:45:49 +0300

    Accordion

---
+++ 1-react-hooks-basic-projects/react-demos/8. Accordion/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { accordionData } from "./utils/content.js";
+import Accordion from './components/Accordion.jsx';
+
+const App = () => {
+  return (
+    <div className="accordion">
+{accordionData.map(({ title, content }) => (
+  <Accordion key={Math.random()} title={title} content={content} />
+))}
+
+    </div>
+  );
+};
+
+export default App;
