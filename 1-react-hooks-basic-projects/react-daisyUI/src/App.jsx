ff85c0c9dcc4f6ae2ea99b28cdb6afcd7fc0de92
hishamrehab <hishamrehab30@gmail.com>
2025-11-02 04:06:53 +0200

    Theme Contrller

--- 1-react-hooks-basic-projects/react-daisyUI/src/App.jsx
+++ 1-react-hooks-basic-projects/react-daisyUI/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Buttons from './components/Buttons'
 import Dropdown from './components/Dropdown'
 import Modal from './components/Modal'
+import ThemeController from './components/ThemeController'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <div className="mt-10 flex items-center gap-5">
     {/* <Buttons /> */}
     {/* <Dropdown /> */}
-    <Modal />
+    {/* <Modal /> */}
+    <ThemeController />
     </div>
   )
 }
